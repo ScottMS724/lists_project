a6efdba95cde529c197bdf07643e0fd771987c63
scottms724 <scottms724@gmail.com>
2020-02-23 12:01:54 -0500

    Added status to items and const STATUS.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_200723) do
+ActiveRecord::Schema.define(version: 2020_02_23_164236) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
     t.integer "list_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status", default: false
   end
 
   create_table "lists", force: :cascade do |t|
