923e1c4b56f7ce5c7049c80ad9c2b27449b6386e
scottms724 <scottms724@gmail.com>
2020-02-27 13:54:56 -0500

    Built password functionality for users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_164701) do
+ActiveRecord::Schema.define(version: 2020_02_27_183325) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_164701) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
