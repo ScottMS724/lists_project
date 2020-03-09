1bb747d627679a743e5436ef2d1e78a2f3ba2f90
scottms724 <scottms724@gmail.com>
2020-03-09 16:19:31 -0400

    Can add lists again.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,5 @@
 class List < ApplicationRecord
     has_many :items 
-    belongs_to :user 
     validates :name, :presence => true 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_secure_password 
-    has_many :lists 
     validates :email, :presence => true 
     validates :email, :uniqueness => true 
 
