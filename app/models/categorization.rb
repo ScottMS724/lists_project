ff99c75ac118a9bde4637f444ac128eecedbb360
scottms724 <scottms724@gmail.com>
2020-04-09 16:34:13 -0400

    Item's categorization displays on show page.

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,4 +1,13 @@
 class Categorization < ApplicationRecord
     belongs_to :category
     belongs_to :item
+
+    def item_name
+        item.description
+    end 
+
+    def category_name
+        category.name
+    end 
+
 end
