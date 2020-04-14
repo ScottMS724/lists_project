9b03aa0115a748f5782e611bce6802933bbeea67
scottms724 <scottms724@gmail.com>
2020-04-14 15:38:02 -0400

    Column name fixed in Categorizations table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_164937) do
+ActiveRecord::Schema.define(version: 2020_04_14_190023) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_164937) do
   create_table "categorizations", force: :cascade do |t|
     t.integer "category_id"
     t.integer "item_id"
-    t.boolean "fragile?"
+    t.boolean "fragile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
