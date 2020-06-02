9543cd039c40da4416030d30091a1061bc785ef1
scottms724 <scottms724@gmail.com>
2020-06-02 12:18:38 -0400

    Add dependent destroy to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password 
-    has_many :lists 
+    has_many :lists, dependent: :destroy
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
